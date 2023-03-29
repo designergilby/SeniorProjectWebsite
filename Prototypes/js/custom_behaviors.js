eaeaa6e68265c8910562e662b636cf2e1a0e59f5
designergilby <designergilby@gmail.com>
2023-03-29 13:13:32 -0500

    Updated Prototype

--- Prototypes/js/custom_behaviors.js
+++ Prototypes/js/custom_behaviors.js
@@ -4,7 +4,17 @@ $(document).ready(function() {
 	
 	
 	// EVENT HANDLERS
-	
+	$(".name").on("click", function(event){
+		event.preventDefault()
+		console.log("hit")
+		$(".overlay").fadeIn(200)
+	})
+
+	$(".close").on("click", function(event){
+		event.preventDefault()
+		console.log("hit")
+		$(".overlay").fadeOut(200)
+	})
 	
 	// FUNCTIONS
 	
