c9f9e9fb9238c4c9f908944c2a2bbc1b60715a1d
greenc9182 <greenc9182@my.uwstout.edu>
2023-04-13 19:02:03 -0500

    Nav Bar added

--- js/custom_behaviors.js
+++ js/custom_behaviors.js
@@ -14,6 +14,19 @@ $(document).ready(function() {
 		$(".designerPageHolder ").fadeOut(800)
 	})
 
+	$(".navButton").on("click", function(event){
+		event.preventDefault()
+		$("nav").fadeToggle(500)
+	})
+	
+	$(window).on("resize", function(){
+		if(window.innerWidth >=768){
+			$("nav").css("display", "flex")
+		} else {
+			$("nav").css("display", "none")
+		}
+	})
+
 	
 	
 });
